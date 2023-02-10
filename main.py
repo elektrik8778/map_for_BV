7ba22a8d3f45480e257d837eece12e1d498cfc05
cherepakhin <cherepakhin87@bk.ru>
2023-02-10 09:57:13 +0900

    Чтение координат из базы данных

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ from flask import Flask, render_template_string
 import folium
 from folium.plugins import MousePosition
 from folium.plugins import Draw
-from class1 import LatLngPopup
+# from class1 import LatLngPopup
+
+import texts
+import firebase_admin
+from firebase_admin import db
+
+
+
+
+file_key = "chep-192c7-firebase-adminsdk-2uqrg-fca3a5beed.json"
+databaseURL = "https://chep-192c7.firebaseio.com"
+
+cred_object = firebase_admin.credentials.Certificate(file_key)
+default_app = firebase_admin.initialize_app(cred_object, {
+	'databaseURL':databaseURL
+	})
+
+
+
+# print(ref.order_by_key().get())
+
 
 app = Flask(__name__)
 
@@ -16,8 +36,11 @@ app = Flask(__name__)
 @app.route("/")
 def fullscreen():
     """Simple example of a fullscreen map."""
+    current_position_gps = (db.reference("current_position_gps")).order_by_key().get()
+    # print(f"lon: {float(current_position_gps['lon'])}, lat: {float(current_position_gps['lat'])}  ")
     m = folium.Map(
-        location=[50.5740, 127.6601],
+        # location=[50.5740, 127.6601],
+        location=[float(current_position_gps['lat']), float(current_position_gps['lon'])],
         zoom_start=18,
     )
 
@@ -34,7 +57,7 @@ def fullscreen():
 
 
 
-    formatter = "function(num) {return L.Util.formatNum(num, 5);};"
+    formatter = "function(num) {return L.Util.formatNum(num, 4);};"
 
     draw = Draw(
         draw_options={
@@ -44,8 +67,8 @@ def fullscreen():
             'polygon': True,
             'circle': False,
             'marker': True,
-            'circlemarker': True},
-        edit_options={'edit': True})
+            'circlemarker': False},
+        edit_options={'edit': False})
 
 
     mouse_position = MousePosition(
@@ -65,82 +88,75 @@ def fullscreen():
     folium.LayerControl().add_to(m)
 
     # inject html into the map html
-    m.get_root().html.add_child(folium.Element("""
-    <div style="position: fixed; 
-         top: 50px; left: 70px; width: 150px; height: 70px; 
-         background-color:orange; border:2px solid grey;z-index: 900;">
-        <h5>Hello World!!!</h5>
-        <button>Test Button</button>
-    </div>
-    """))
+    # m.get_root().html.add_child(folium.Element(texts.element_1))
 
 
     return m.get_root().render()
 
 
-@app.route("/iframe")
-def iframe():
-    """Embed a map as an iframe on a page."""
-    m = folium.Map(
-        location=[50.260924, 127.557525],
-        zoom_start=13,
-    )
+# @app.route("/iframe")
+# def iframe():
+#     """Embed a map as an iframe on a page."""
+#     m = folium.Map(
+#         location=[50.260924, 127.557525],
+#         zoom_start=13,
+#     )
+#
+#     # set the iframe width and height
+#     m.get_root().width = "800px"
+#     m.get_root().height = "600px"
+#     iframe = m.get_root()._repr_html_()
+#
+#     return render_template_string(
+#         """
+#             <!DOCTYPE html>
+#             <html>
+#                 <head></head>
+#                 <body>
+#                     <h1>Using an iframe</h1>
+#                     {{ iframe|safe }}
+#                 </body>
+#             </html>
+#         """,
+#         iframe=iframe,
+#     )
 
-    # set the iframe width and height
-    m.get_root().width = "800px"
-    m.get_root().height = "600px"
-    iframe = m.get_root()._repr_html_()
 
-    return render_template_string(
-        """
-            <!DOCTYPE html>
-            <html>
-                <head></head>
-                <body>
-                    <h1>Using an iframe</h1>
-                    {{ iframe|safe }}
-                </body>
-            </html>
-        """,
-        iframe=iframe,
-    )
-
-
-@app.route("/components")
-def components():
-    """Extract map components and put those on a page."""
-    m = folium.Map(
-        location=[50.260924, 127.557525],
-        zoom_start=13,
-        width=800,
-        height=600
-    )
-
-    m.get_root().render()
-    header = m.get_root().header.render()
-    body_html = m.get_root().html.render()
-    script = m.get_root().script.render()
-
-    return render_template_string(
-        """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    {{ header|safe }}
-                </head>
-                <body>
-                    <h1>Using components</h1>
-                    {{ body_html|safe }}
-                    <script>
-                        {{ script|safe }}
-                    </script>
-                </body>
-            </html>
-        """,
-        header=header,
-        body_html=body_html,
-        script=script,
-    )
+# @app.route("/components")
+# def components():
+#     """Extract map components and put those on a page."""
+#     m = folium.Map(
+#         location=[50.260924, 127.557525],
+#         zoom_start=13,
+#         width=800,
+#         height=600
+#     )
+#
+#     m.get_root().render()
+#     header = m.get_root().header.render()
+#     body_html = m.get_root().html.render()
+#     script = m.get_root().script.render()
+#
+#     return render_template_string(
+#         """
+#             <!DOCTYPE html>
+#             <html>
+#                 <head>
+#                     {{ header|safe }}
+#                 </head>
+#                 <body>
+#                     <h1>Using components</h1>
+#                     {{ body_html|safe }}
+#                     <script>
+#                         {{ script|safe }}
+#                     </script>
+#                 </body>
+#             </html>
+#         """,
+#         header=header,
+#         body_html=body_html,
+#         script=script,
+#     )
 
 
 if __name__ == "__main__":
---
+++ texts.py
@@ -0,0 +1,10 @@
+element_1 = """
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <div style="position: fixed; 
+         top: 50px; left: 70px; width: 150px; height: 70px; 
+         background-color:orange; border:2px solid grey;z-index: 900;">
+         <button class="btn btn-primary" type="submit">Button1</button>
+         <button class="btn btn-primary" type="submit">Button2</button>
+         <button class="btn btn-primary" type="submit">Button3</button>
+    </div>
+    """
